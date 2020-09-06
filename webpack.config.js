cc0b39a808226f94a1230553d8fd595c17d7b78f
AtharvRedij <atharvaredij@gmail.com>
2020-09-06 18:13:02 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  devServer: {
+    stats: "errors-only",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+};
